6be70b7ae5945a6ffe7c9426d89de2d2ab5f3a79
ViktoriaAY <vetska@vk.com>
2026-05-28 16:25:12 +0300

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Виктория Юношева on 28.05.2026.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
